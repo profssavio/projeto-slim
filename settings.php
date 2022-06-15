c08e841ad2dad2ab8169afb78c0845b4365eac95
Sávio Silva <savio@pop-os.localdomain>
2022-06-14 22:00:32 -0300

    ajustes

--- settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$container['logger'] = function ( $c ) {
-    $logger       = new \Monolog\Logger( 'my_logger' );
-    $file_handler = new \Monolog\Handler\StreamHandler( '../logs/app.log' );
-    $logger->pushHandler( $file_handler );
-    return $logger;
-};
